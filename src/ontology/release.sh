773ef67e7c41eef975a19f3708842dc1ae4cc46a
joergwa <waitelonis@yovisto.com>
2025-10-21 13:43:28 +0200

    Update release.sh

--- src/ontology/release.sh
+++ src/ontology/release.sh
@@ -1,21 +1,21 @@
 
 sh run.sh prepare_release
 
-sh run.sh make skos-bb.ttl
-sh run.sh make skos-be.ttl
+#sh run.sh make skos-bb.ttl
+#sh run.sh make skos-be.ttl
 sh run.sh make skos-bw.ttl
-sh run.sh make skos-by.ttl
+#sh run.sh make skos-by.ttl
 sh run.sh make skos-hb.ttl
 sh run.sh make skos-he.ttl
-sh run.sh make skos-hh.ttl
+#sh run.sh make skos-hh.ttl
 sh run.sh make skos-mv.ttl
 sh run.sh make skos-ni.ttl
 sh run.sh make skos-nw.ttl
-sh run.sh make skos-rp.ttl
+s#h run.sh make skos-rp.ttl
 sh run.sh make skos-sh.ttl
 sh run.sh make skos-sl.ttl
-sh run.sh make skos-sn.ttl
+#sh run.sh make skos-sn.ttl
 sh run.sh make skos-st.ttl
-sh run.sh make skos-th.ttl
+#sh run.sh make skos-th.ttl
 
 mv skos-* ../../skos/
